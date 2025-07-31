99c9bfc9c5036259dca3c2ce9fed96118092e88a
swapbamnote <swapbamnote@gmail.com>
2025-07-31 11:03:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 import { Player } from 'https://e-qa1.video-cdn.net/mi-player-sdk-qa/mi-player-sdk.js';
 
 const player = new Player('playerfr');
-player.autoplay()
-        .then(() => {
-            console.log("video playing")
-    })
-.catch((error)=>{console.log("error",error)});
 
 
 const playBtn = document.querySelector("#play");
